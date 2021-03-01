695c1cfb8747e571464e4396b7307b250133b1de
gracefurya <gfuruyavaldivia@gmail.com>
2021-02-28 23:12:47 -0400

    intento de cargar imagen 1

--- app/src/main/java/com/example/arbolito/AgregarArbol.java
+++ app/src/main/java/com/example/arbolito/AgregarArbol.java
@@ -2,17 +2,26 @@ package com.example.arbolito;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 
 import com.google.gson.Gson;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -30,12 +39,15 @@ public class AgregarArbol extends AppCompatActivity {
     private ZonaVerde[] zonasverdes;
     private ZonaVerde zonaverdeSeleccionada;
 
+    private ImageView image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agregar_arbol);
         spinnerDistritos=findViewById(R.id.spinner);
         spinnerZonasVerdes=findViewById(R.id.spinner2);
+        image=findViewById(R.id.imageView);
         LlenarDistrito();
     }
 
@@ -132,4 +144,55 @@ public class AgregarArbol extends AppCompatActivity {
             }
         });
     }
+
+    public void agregarArbolitoImagen(View view){
+        Intent intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/");
+        startActivityForResult(intent.createChooser(intent,"Seleccione una Imagen"),10);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode==RESULT_OK){
+            Uri path=data.getData();
+            System.out.println("direccion: "+path.getPath());
+            image.setImageURI(path);
+            subirImagen(path);
+        }
+    }
+
+    public void subirImagen(Uri path){
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl(URLApi)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        arbolitoAPI aa=retrofit.create(arbolitoAPI.class);
+
+        File file = new File("external/images/media/Phalaenopsis_JPEG.png");
+        System.out.println(file.getName());
+        RequestBody requestFile =
+                RequestBody.create( MediaType.parse(getContentResolver().getType(path)),
+                        file);
+        MultipartBody.Part body =
+                MultipartBody.Part.createFormData("picture", file.getName(), requestFile);
+        String descriptionString = "hello, this is description speaking";
+        RequestBody description =RequestBody.create(
+                        okhttp3.MultipartBody.FORM, descriptionString);
+        Call<ResponseBody> call=aa.updateProfile(body);
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                System.out.println("naaa");
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/example/arbolito/arbolitoAPI.java
+++ app/src/main/java/com/example/arbolito/arbolitoAPI.java
@@ -1,9 +1,16 @@
 package com.example.arbolito;
 
+import android.database.Observable;
+
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface arbolitoAPI {
@@ -21,4 +28,10 @@ public interface arbolitoAPI {
 
     @GET("/api/v1/zonaverde/distrito/{iddistrito}")
     public Call<ResponseZonaVerde> obtenerZonasVerdesByIDDistrito(@Path("iddistrito") int iddistrito);
+
+    @Multipart
+    @POST("/api/v1/arbolito")
+    public Call<ResponseBody> updateProfile(
+                                           @Part MultipartBody.Part image);
+
 }
